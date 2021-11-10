26ef168406b3d5d361594fcf0c44d2942f645e94
Paulo <paulo_piotto@hotmail.com>
2021-11-10 18:34:41 -0300

    script de teste corrigido

--- src/database/database.js
+++ src/database/database.js
@@ -3,11 +3,11 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const connection = new Pool({
-    host: process.env.DB_HOST,
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  port: parseInt(process.env.DB_PORT),
-  database: process.env.DB_DATABASE
+  port: parseInt(process.env.DB_PORT, 10),
+  database: process.env.DB_DATABASE,
 });
 
 export default connection;
--- tests/categories.test.js
+++ tests/categories.test.js
@@ -2,7 +2,7 @@
 import supertest from 'supertest';
 import app from '../src/app.js';
 
-describe('GET /products', () => {
+describe('GET /categories', () => {
   it('always returns 200', async () => {
     const result = await supertest(app).get('/categories');
     const { status } = result;

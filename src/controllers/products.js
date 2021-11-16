fa30ece111d217895a6cb86ba8556972c41075fb
Pedro Hotz <pedrohotz2002@gmail.com>
2021-11-16 12:25:34 -0300

    testing

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -3,6 +3,7 @@ import connection from '../database/database.js';
 async function getProducts(req, res) {
   try {
     const result = await connection.query('SELECT * FROM product');
+    console.log(result.rows);
     res.send(result.rows);
   } catch (error) {
     res.sendStatus(500);

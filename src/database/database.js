1b4b195e952ec16ad484b074542a63ca3c71b5c1
Pedro Hotz <pedrohotz2002@gmail.com>
2021-11-16 11:03:47 -0300

    Setup for deploy

--- src/database/database.js
+++ src/database/database.js
@@ -2,12 +2,14 @@ import pg from 'pg';
 
 const { Pool } = pg;
 
-const connection = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  port: parseInt(process.env.DB_PORT, 10),
-  database: process.env.DB_DATABASE,
-});
+const databaseConfig = {
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+      rejectUnauthorized: false
+  }
+}
+
+
+const connection = new Pool(databaseConfig);
 
 export default connection;
--- src/server.js
+++ src/server.js
@@ -1,8 +1,7 @@
 import './setup.js';
 import app from './app.js';
 
-const port = 4000;
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}.`);
+app.listen(process.env.PORT, () => {
+  console.log("Server is listening on port" + process.env.PORT);
 });
